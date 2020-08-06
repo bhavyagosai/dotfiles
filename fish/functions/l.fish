153ad2e1955f6be0282a831a3dde19d3b1ad4583
henild <dedaniahenil@gmail.com>
2020-08-06 21:58:29 +0530

    update fish functions to add exa

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
 function l --wraps='ls -lah' --description 'alias l ls -lah'
+  if which exa &> /dev/null
+  exa -lahF $argv
+  else
   ls -lahF $argv;
+  end
 end
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
 function ls --wraps='ls -hN --color=auto --group-directories-first' --description 'alias ls=ls -hN --color=auto --group-directories-first'
- command ls -hNF --color=auto --group-directories-first $argv;
+  if which exa &> /dev/null
+    command exa -hF
+  else
+    command ls -hNF --color=auto --group-directories-first $argv;
+  end
 end
