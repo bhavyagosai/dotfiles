f6ecccf8c1244c6c03cc9924501c2cbfd6ea723c
henild <dedaniahenil@gmail.com>
2020-08-22 17:35:16 +0530

    update fish functions

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,7 +1,7 @@
 # Defined in - @ line 1
 function l --wraps='ls -lah' --description 'alias l ls -lah'
   if which exa &> /dev/null
-    exa -lhF --group-directories-first $argv
+    exa -lhF --sort=extension --group-directories-first $argv
   else
     ls -lhF $argv;
   end
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,8 +1,8 @@
 # Defined in - @ line 1
 function ls --wraps='ls -hN --color=auto --group-directories-first' --description 'alias ls=ls -hN --color=auto --group-directories-first'
   if which exa &> /dev/null
-    exa -hF --group-directories-first $argv
+    exa -hF --sort=extension --group-directories-first $argv
   else
-    ls -hNF --color=auto --group-directories-first $argv;
+    ls -hNF --color=auto --sort=extension $argv;
   end
 end
