4cd42b8916e8917ea86c902a22b96e41c18724a9
henil <dedaniahenil@gmail.com>
2020-10-07 13:27:56 +0530

    fish: minor update

--- fish/functions/gco.fish
+++ fish/functions/gco.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gco --wraps='git commit' --description 'alias gco=git commit'
-  git commit $argv;
+function gco --wraps='git commit' --wraps='git checkout' --description 'alias gco=git checkout'
+  git checkout $argv;
 end
